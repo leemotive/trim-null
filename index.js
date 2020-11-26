a5d98cf6e0b3ef8a917d5df1bd66e9a6e8d6542d
leemotive <wstation@163.com>
2020-11-26 16:36:15 +0800

    format

--- index.js
+++ index.js
@@ -4,19 +4,20 @@ export default function trimNull(obj) {
     return;
   }
 
-  const trimedObj = JSON.parse(str, function(k, v) {
+  const trimedObj = JSON.parse(str, function (k, v) {
     if (v === null) {
       return;
     }
     if (Array.isArray(v)) {
-      for(let i = 0; i < v.length; i++) {
+      for (let i = 0; i < v.length; i++) {
         if (v[i] == undefined) {
           // 主要是empty换为undefined, 方便结果可以使用map等方法遍历到这一项
-          v[i] = void 0
+          v[i] = void 0;
         }
       }
     }
     return v;
-  })
+  });
+
   return trimedObj;
 }
